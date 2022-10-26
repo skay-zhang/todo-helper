3dab07a02357d8d03283101c80e57969bc545316
skay <skai-zhang@hotmail.com>
2022-10-26 11:43:54 +0800

    Initial commit

---
+++ basic/app.js
@@ -0,0 +1,114 @@
+process.env.DIST_ELECTRON = join(__dirname, '..')
+process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : join(process.env.DIST_ELECTRON, '../public')
+
+import { app, BrowserWindow, globalShortcut, shell, ipcMain } from 'electron'
+import http from './module/http-service'
+import { release } from 'os'
+import { join } from 'path'
+
+// 禁用Windows7的GPU加速
+if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+// 设置Windows10+应用名称
+if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+// 阻止应用程序多开
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
+// 屏蔽Electron安全警告
+// process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+let win = null
+const preload = join(__dirname, './preload.js')
+const indexHtml = join(process.env.DIST, 'index.html')
+const url = process.env.VITE_DEV_SERVER_URL
+// 创建窗口
+async function createWindow() {
+  win = new BrowserWindow({
+    title: 'Main window',
+    icon: join(process.env.PUBLIC, 'favicon.ico'),
+    webPreferences: {
+      preload,
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  if (app.isPackaged) {
+    win.loadFile(indexHtml)
+  } else {
+    win.loadURL(url)
+    // Open devTool if the app is not packaged
+    win.webContents.openDevTools()
+  }
+
+  // Test actively push message to the Electron-Renderer
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  })
+
+  // Make all links open with the browser, not with the application
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith('https:')) shell.openExternal(url)
+    return { action: 'deny' }
+  })
+}
+// 应用初始化
+async function init(){
+  globalShortcut.register('Ctrl+CommandOrControl+T', () => {
+    console.log('[app] Listened to shortcut keys')
+  });
+  http.start(app.isPackaged);
+  console.log('[app] Register shortcuts')
+  createWindow();
+}
+// 应用就绪
+app.whenReady().then(init)
+
+// ==================== 以下为事件监听部分 ==================== //
+
+// 窗口全部关闭
+app.on('window-all-closed', () => {
+  win = null
+  if (process.platform !== 'darwin') app.quit()
+})
+// 尝试关闭应用
+app.on('before-quit', () => {
+  http.stop();
+  // 注销快捷键
+  globalShortcut.unregisterAll()
+  console.log('[app] Unregister shortcuts')
+})
+// 第二个实例启动
+app.on('second-instance', () => {
+  if (win) {
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+// 激活应用实例
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows()
+  if (allWindows.length) allWindows[0].focus()
+  else createWindow()
+})
+// 打开子窗口
+ipcMain.handle('open-win', (event, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  if (app.isPackaged) childWindow.loadFile(indexHtml, { hash: arg })
+  else {
+    childWindow.loadURL(`${url}#${arg}`)
+    // childWindow.webContents.openDevTools({ mode: "undocked", activate: true })
+  }
+})
---
+++ basic/module/http-service.js
@@ -0,0 +1,96 @@
+import express from 'express'
+import path from 'path'
+import http from 'http'
+
+// 服务对象
+let Server;
+// 会话连接池
+let Sockets = [];
+
+const httpService = {
+  port: 22333,
+  state: false,
+  start: dev => {
+    console.log('[http] Service start on port ' + httpService.port)
+    Server = http.createServer(controller(dev)).listen(httpService.port, () => {
+      console.log('Service Running')
+      httpService.state = true
+    })
+    Server.on("connection", function (socket) {
+      let ip = socket.remoteAddress;
+      // 仅允许本地访问
+      if (ip === '::1' || ip === '::ffff:127.0.0.1') {
+        Sockets.push(socket);
+        socket.once("close", function () {
+          Sockets.splice(Sockets.indexOf(socket), 1);
+        });
+      } else socket.destroy();
+    });
+  },
+  stop: () => {
+    console.log('[http] Service ready to stop, end ' + Sockets.length + ' connections')
+    // 关闭连接
+    Sockets.forEach(function (socket) {
+      socket.destroy();
+    });
+    // 停止服务
+    if (Server) {
+      Server.close(() => {
+        console.log('[http] Service stop')
+        httpService.state = false
+        Sockets = []
+        Server = {}
+      });
+    }
+  }
+}
+
+// 请求控制器
+function controller(dev) {
+  let serve = express()
+  // 全局拦截
+  serve.all("*", (req, res, next) => {
+    // 开发时开启跨域
+    if (dev) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'content-type');
+      res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    }
+    if (req.method.toLowerCase() == 'options') res.send(200);
+    else next();
+  })
+  // 初始化添加页面
+  serve.get('/api/add', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  // 添加事项
+  serve.post('/api/add', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  // 获取事项列表
+  serve.get('/api/list', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  // 获取事项详情
+  serve.get('/api/item/*', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  // 编辑事项
+  serve.post('/api/item/*', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  // 移除事项
+  serve.delete('/api/item/*', (_req, res) => {
+    ret(res, true, 'ok')
+  });
+  serve.use(express.static(path.join(__dirname, dev ? '../client' : '../../public/client')))
+  return serve;
+}
+
+function ret(res, state, message) {
+  res.setHeader("Content-Type", "application/json;charset=UTF-8")
+  res.send({ state, message })
+  return false;
+}
+
+export default httpService
